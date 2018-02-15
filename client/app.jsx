1539d0479d2005b071bd6a5953a76d700c5e0929
kguinto <klogold1@gmail.com>
2018-02-15 15:02:45 -0800

    Refactored 'rounds' to 'frames'

--- client/app.jsx
+++ client/app.jsx
@@ -1,32 +1,36 @@
 class App extends React.Component {
   constructor (props) {
     super(props);
-    let blankRounds = [];
-    let roundTotals = [];
+    let blankFrames = [];
+    let frameTotals = [];
 
     for (let i = 0; i < 10; i++) {
-      blankRounds.push([0, -1, 0]);
-      roundTotals.push(1);
+      blankFrames.push([0, -1, 0]);
+      frameTotals.push(1);
     }
 
     this.state = {
-      rounds: blankRounds,
-      roundTotals: roundTotals
+      frames: blankFrames,
+      frameTotals: frameTotals
     };
   }
 
-  resetRounds () {
-    let blankRounds = [];
-    let roundTotals = [];
+  bowl(pins) {
+
+  }
+
+  resetFrames () {
+    let blankFrames = [];
+    let frameTotals = [];
 
     for (let i = 0; i < 10; i++) {
-      blankRounds.push([-1, -1, -1]);
-      roundTotals.push(-1);
+      blankFrames.push([-1, -1, -1]);
+      frameTotals.push(-1);
     }
 
     this.setState = {
-      rounds: blankRounds,
-      roundTotals: roundTotals
+      frames: blankFrames,
+      frameTotals: frameTotals
     };
   }
 
@@ -34,7 +38,7 @@ class App extends React.Component {
     return (
     <div>
       <h1>BBBBBOWLING</h1>
-      <Scoreboard rounds={this.state.rounds} totals={this.state.roundTotals}/>
+      <Scoreboard frames={this.state.frames} totals={this.state.frameTotals}/>
     </div>
     );
   }
@@ -43,29 +47,29 @@ class App extends React.Component {
 var Scoreboard = (props) => {
   return (
   <div class="scoreboard">
-    {props.rounds
-      .map((rounds, index) => (<Round round={props.rounds[index]} number={index} total={props.totals[index]}/>))
+    {props.frames
+      .map((frames, index) => (<Frame frame={props.frames[index]} number={index} total={props.totals[index]}/>))
     }
 
   </div>
   );
 }
 
-var Round = (props) => {
-  console.log(props.round);
-  var bowl1 = (props.round[0] >= 0) ? props.round[0] : ' ';
-  var bowl2 = (props.round[1] >= 0) ? props.round[1] : ' ';
+var Frame = (props) => {
+  console.log(props.frame);
+  var bowl1 = (props.frame[0] >= 0) ? props.frame[0] : ' ';
+  var bowl2 = (props.frame[1] >= 0) ? props.frame[1] : ' ';
 
-  var bowl3 = (props.round[2] >= 0) ? props.round[2] : ' ';
+  var bowl3 = (props.frame[2] >= 0) ? props.frame[2] : ' ';
 
   var total = (props.total >= 0) ? props.total : '';
 
   return (
-    <div class={(props.number === 9) ? "round round10" : "round"}>
+    <div class={(props.number === 9) ? "frame frame10" : "frame"}>
       <div class="bowl1">{bowl1}</div>
       <div class="bowl2">{bowl2}</div>
-      {(props.number === 9) ? <div class="bowl3" isTenthRound={false}>{bowl3}</div> : <div />}
-      <div class="roundTotal">{total}</div>
+      {(props.number === 9) ? <div class="bowl3" isTenthFrame={false}>{bowl3}</div> : <div />}
+      <div class="frameTotal">{total}</div>
     </div>
     );
 }
