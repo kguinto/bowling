739c85f70e3df14b422d869f08540f4db59e1f5b
kguinto <klogold1@gmail.com>
2018-02-15 14:55:58 -0800

    Implemented model for rounds and integrated with round instances

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,39 @@
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+
+    let blankRounds = [];
+    let roundTotals = [];
+
+    for (let i = 0; i < 10; i++) {
+      blankRounds.push([0, -1, 0]);
+      roundTotals.push(1);
+    }
+
+    this.state = {
+      rounds: blankRounds,
+      roundTotals: roundTotals
+    };
+  }
+
+  resetRounds () {
+    let blankRounds = [];
+    let roundTotals = [];
+
+    for (let i = 0; i < 10; i++) {
+      blankRounds.push([-1, -1, -1]);
+      roundTotals.push(-1);
+    }
+
+    //setstate
+  }
+
   render () {
     return (
     <div>
       <h1>BBBBBOWLING</h1>
-      <Scoreboard />
+      <Scoreboard rounds={this.state.rounds} totals={this.state.roundTotals}/>
     </div>
     );
   }
@@ -12,8 +42,8 @@ class App extends React.Component {
 var Scoreboard = (props) => {
   return (
   <div class="scoreboard">
-    {(new Array(10)).fill(0)
-      .map((score, index) => (<Round score={score} number={index}/>))
+    {props.rounds
+      .map((rounds, index) => (<Round round={props.rounds[index]} number={index} total={props.totals[index]}/>))
     }
 
   </div>
@@ -21,13 +51,20 @@ var Scoreboard = (props) => {
 }
 
 var Round = (props) => {
+  console.log(props.round);
+  var bowl1 = (props.round[0] >= 0) ? props.round[0] : ' ';
+  var bowl2 = (props.round[1] >= 0) ? props.round[1] : ' ';
+
+  var bowl3 = (props.round[2] >= 0) ? props.round[2] : ' ';
+
+  var total = (props.total >= 0) ? props.total : '';
 
   return (
     <div class={(props.number === 9) ? "round round10" : "round"}>
-      <div class="bowl1">7</div>
-      <div class="bowl2">2</div>
-      {(props.number === 9) ? <div class="bowl3" isTenthRound={false}>0</div> : <div />}
-      <div class="roundTotal">9</div>
+      <div class="bowl1">{bowl1}</div>
+      <div class="bowl2">{bowl2}</div>
+      {(props.number === 9) ? <div class="bowl3" isTenthRound={false}>{bowl3}</div> : <div />}
+      <div class="roundTotal">{total}</div>
     </div>
     );
 }
